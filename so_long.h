465e99e0589dc597dace63593a5a43365a0d5ef3
Abderrahmane Ababachir <aababach@e1r11p9.1337.ma>
2022-06-19 19:11:48 +0100

    work

--- so_long.h
+++ so_long.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aababach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/19 19:10:10 by aababach          #+#    #+#             */
+/*   Updated: 2022/06/19 19:11:43 by aababach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 #include <mlx.h>
@@ -7,30 +19,30 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-struct elements{
-    int E;
-    int C;
-    int P;
+struct	elements{
+	int	E;
+	int	C;
+	int	P;
 };
 void	put_nbr(int n);
-void    ft_error();
-typedef struct p{
-    void    *ptr;
-    void    *w_ptr;
-    void    *player;
-    void    *backg;
-    void    *obs;
-    void    *coin;
-    void    *ex;
-    int     n_coin;
-    int     x;
-    int     y;
-    char    **map;
-    int     n_moves;
+void	ft_error();
+typedef struct	p{
+	void	*ptr;
+	void	*w_ptr;
+	void	*player;
+	void	*backg;
+	void	*obs;
+	void	*coin;
+	void	*ex;
+	int		n_coin;
+	int		x;
+	int		y;
+	char	**map;
+	int		n_moves;
 }ptr;
-int     valid_move(char **map, int x, int y, ptr *l);
-int     lines(char *name);
-int     x_count(char *name);
-void    str_map(char *name, ptr *l);
-void    double_free(char **str, int y);
+int	valid_move(char **map, int x, int y, ptr *l);
+int	lines(char *name);
+int	x_count(char *name);
+void	str_map(char *name, ptr *l);
+void	double_free(char **str, int y);
 #endif
